ac7f61f4b4fbcd5e53805c8fe6a068abd522d858
mhmmtkkl <makoklu32@gmail.com>
2020-04-25 15:55:17 -0400

    keywordDriven

---
+++ src/test/java/Runner/smokeTestRunner.java
@@ -0,0 +1,20 @@
+package Runner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin={"html:target/cucumber-report" , "json:target/cucumber.json"},
+        tags = "@SmokeTest",
+        features = {"src/test/resources"},
+        glue = {"stepDefinition"},
+        dryRun = false
+)
+
+public class smokeTestRunner {
+
+//    4 pm
+
+}
